2a148a3aeac8d8b3ac2c6457ffff4f954fd567a9
vjsantojaca <victorsantoja@gmail.com>
2018-09-23 20:23:05 +0200

    Fixed some problems

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 # local imports
 from config import app_config
+# from models import Information
 
 # db variable initialization
 db = SQLAlchemy()
@@ -22,7 +23,7 @@ def create_app(config_name):
         return 'Hello, World!'
 
     @app.route("/temp_now")
-    def lab_temp():
+    def temp_now():
         import sys
         import Adafruit_DHT
         humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 2)
@@ -30,5 +31,36 @@ def create_app(config_name):
             return render_template("temp_now.html",temp=temperature,hum=humidity).encode("utf-8")
         else:
             return render_template("problems_sorry.html")
+
+    @app.route("/temp_historic", methods=['GET'])
+    def temp_historic():
+        from_date_str     = request.args.get('from',time.strftime("%Y-%m-%d 00:00")) #Get the from date value from the URL
+        to_date_str       = request.args.get('to',time.strftime("%Y-%m-%d %H:%M"))   #Get the to date value from the URL
+        range_h_form      = request.args.get('range_h','');  #This will return a string, if field range_h exists in the request
+        range_h_int       = "nan"  #initialise this variable with not a number
+        
+        
+        # temperatures, humidities, timezone, from_date_str, to_date_str = 
+        # Create new record tables so that datetimes are adjusted back to the user browser's time zone.
+        # time_adjusted_temperatures = []
+        # time_adjusted_humidities   = []
+        # for record in temperatures:
+          #  local_timedate = arrow.get(record[0], "YYYY-MM-DD HH:mm").to(timezone)
+           # time_adjusted_temperatures.append([local_timedate.format('YYYY-MM-DD HH:mm'), round(record[2],2)])
+
+        # for record in humidities:
+          #  local_timedate = arrow.get(record[0], "YYYY-MM-DD HH:mm").to(timezone)
+          #  time_adjusted_humidities.append([local_timedate.format('YYYY-MM-DD HH:mm'), round(record[2],2)])
+
+        # print "rendering lab_env_db.html with: %s, %s, %s" % (timezone, from_date_str, to_date_str)
+
+        # return render_template("lab_env_db.html", timezone        = timezone,
+          #                                        temp            = time_adjusted_temperatures,
+           #                                       hum             = time_adjusted_humidities, 
+            #                                      from_date       = from_date_str, 
+             #                                     to_date         = to_date_str,
+              #                                    temp_items      = len(temperatures),
+               #                                   query_string    = request.query_string,
+                #                                  hum_items       = len(humidities))
 
     return app
