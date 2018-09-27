e05ba01718693da4511e97ec230cb6f12104de73
vjsantojaca <victorsantoja@gmail.com>
2018-09-27 20:02:52 +0200

    Fixed some problems

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@
 
 # third-party imports
 from flask import Flask
+from flask import request
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
 
@@ -32,36 +33,35 @@ def create_app(config_name):
         else:
             return render_template("problems_sorry.html")
 
-    @app.route("/temp_historic", methods=['GET'])
+    @app.route("/temp_historic", methods=['GET', 'POST'])
     def temp_historic():
-        return render_template("temp_historic.html")
-        # from_date_str     = request.args.get('from',time.strftime("%Y-%m-%d 00:00")) #Get the from date value from the URL
-        # to_date_str       = request.args.get('to',time.strftime("%Y-%m-%d %H:%M"))   #Get the to date value from the URL
-        # range_h_form      = request.args.get('range_h','');  #This will return a string, if field range_h exists in the request
-        #  range_h_int       = "nan"  #initialise this variable with not a number
+        if request.method == 'GET':
+            return render_template("temp_historic.html")
+            
+        from_date_str     = request.form.get('from',time.strftime("%Y-%m-%d 00:00")) #Get the from date value from the URL
+        to_date_str       = request.form.get('to',time.strftime("%Y-%m-%d %H:%M"))   #Get the to date value from the URL
+        range_h_form      = request.form.get('range_h','');  #This will return a string, if field range_h exists in the request
+        range_h_int       = "nan"  #initialise this variable with not a number
+                
+        temperatures = []
+        humidities = []
         
-        
-        # temperatures, humidities, timezone, from_date_str, to_date_str = 
-        # Create new record tables so that datetimes are adjusted back to the user browser's time zone.
-        # time_adjusted_temperatures = []
-        # time_adjusted_humidities   = []
-        # for record in temperatures:
-          #  local_timedate = arrow.get(record[0], "YYYY-MM-DD HH:mm").to(timezone)
-           # time_adjusted_temperatures.append([local_timedate.format('YYYY-MM-DD HH:mm'), round(record[2],2)])
+        time_adjusted_temperatures = []
+        time_adjusted_humidities   = []
+        for record in temperatures:
+            local_timedate = arrow.get(record[0], "YYYY-MM-DD HH:mm").to(timezone)
+            time_adjusted_temperatures.append([local_timedate.format('YYYY-MM-DD HH:mm'), round(record[2],2)])
 
-        # for record in humidities:
-          #  local_timedate = arrow.get(record[0], "YYYY-MM-DD HH:mm").to(timezone)
-          #  time_adjusted_humidities.append([local_timedate.format('YYYY-MM-DD HH:mm'), round(record[2],2)])
+        for record in humidities:
+            local_timedate = arrow.get(record[0], "YYYY-MM-DD HH:mm").to(timezone)
+            time_adjusted_humidities.append([local_timedate.format('YYYY-MM-DD HH:mm'), round(record[2],2)])
 
-        # print "rendering lab_env_db.html with: %s, %s, %s" % (timezone, from_date_str, to_date_str)
+        print "rendering temp_historic.html with: %s, %s, %s" % (timezone, from_date_str, to_date_str)
 
-        # return render_template("lab_env_db.html", timezone        = timezone,
-          #                                        temp            = time_adjusted_temperatures,
-           #                                       hum             = time_adjusted_humidities, 
-            #                                      from_date       = from_date_str, 
-             #                                     to_date         = to_date_str,
-              #                                    temp_items      = len(temperatures),
-               #                                   query_string    = request.query_string,
-                #                                  hum_items       = len(humidities))
-
+        return render_template("temp_historic.html", timezone        = timezone,
+                                                     temp            = time_adjusted_temperatures,
+                                                     hum             = time_adjusted_humidities, 
+                                                     from_date       = from_date_str, 
+                                                     to_date         = to_date_str)
+    
     return app
